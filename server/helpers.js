c397a76a988359bc83f202d74d6b2fde07472da0
fig <arielfishkin@gmail.com>
2023-04-12 20:33:07 -0700

    removed `pool.connect()` and added `loader.io` routes

--- server/helpers.js
+++ server/helpers.js
@@ -1,195 +1,112 @@
-/* eslint-disable camelcase */
-/* eslint-disable indent */
-require ('dotenv').config();
-const path = require('path');
-const { Pool, Client } = require('pg');
-const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT
+require("dotenv").config();
+const express = require('express');
+const app = express();
+const port = 3000;
+const { fetchReviews } = require('./helpers.js');
+const { fetchMeta } = require('./helpers.js');
+const { insertReview } = require('./helpers.js');
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+//app.use(express.static('public'));
+
+
+//ROUTES
+
+app.get('/loaderio-d3f911b8318a1acd79736acad0061bcf.txt', (req, res) => {
+  res.send('loaderio-d3f911b8318a1acd79736acad0061bcf');
 });
 
-//FETCH REVIEWS FOR A GIVEN PRODUCT
-var fetchReviews = (callback, productId, sort, count, page) => {
-  pool.connect();
-
-  if (count) {
-    var resultCount = count;
-  } else {
-    var resultCount = 5;
-  }
-  if (page) {
-    var resultPage = page;
-  } else {
-    var resultPage = 1;
-  }
-  if (resultPage === 1) {
-    var offSet = 0;
-  } else if (resultPage >= 2) {
-    var offSet = resultCount * (resultPage - 1);
-  }
-
-  if (sort === 'newest' || !sort) {
-    var orderBy = 'date';
-  } else if (sort === 'helpfulness') {
-    var orderBy = 'helpfulness';
-  }
-
-  pool.query(`SELECT * FROM reviews
-  ORDER BY ${orderBy} DESC
-  LIMIT ${resultCount}
-  OFFSET ${offSet}`)
-    .then((result) => {
-      var data = result.rows;
-      var results = {
-        'product': productId,
-        'page': resultPage,
-        'count': resultCount,
-        'results': data
-      };
-      callback(null, results);
-      pool.end();
-    })
-    .catch((err) => {
-      callback(err);
-    });
-};
-
-//FETCH REVIEW METADATA FOR A GIVEN PRODUCT
-var fetchMeta = (prodId, callback) => {
-  pool.connect();
-  var metadata = {
-    'product_id': prodId,
-    'ratings': {},
-    'recommended': {},
-    'characteristics': {}
-  };
-  //POPULATE CHARACTERISTICS OBJECT
-  pool.query(`SELECT characteristic_id, AVG(value), name, reviews.product_id
-  FROM characteristic_review
-  JOIN characteristics ON characteristic_id = characteristics.id
-  JOIN reviews ON review_id = reviews.id
-  WHERE reviews.product_id = 20
-  GROUP BY characteristic_id, name, reviews.product_id`, (err, result) => {
+app.get('/reviews', (req, res) => {
+  var prodId = req.query.product_id;
+  var sortBy = req.query.sort;
+  var resultCount = req.query.count;
+  var resultPages = req.query.page;
+  fetchReviews((err, result) => {
     if (err) {
-      callback(err);
+      console.log(err);
+      res.sendStatus(400);
     } else {
-      var charRows = result.rows;
-      var metaChars = metadata['characteristics'];
-      for (var i = 0; i < charRows.length; i++) {
-        var currentChar = charRows[i];
-        metaChars[currentChar.name] = {
-          'id': currentChar.characteristic_id,
-          'value': currentChar.avg
-        };
-      }
-      //POPULATE RATINGS OBJECT
-      pool.query(`SELECT COUNT (*) FROM reviews WHERE product_id = ${prodId} AND rating = 0`, (err, result) => {
-        if (err) {
-          console.log(error);
-        } else {
-          metadata['ratings']['0'] = result.rows[0].count;
-          pool.query(`SELECT COUNT (*) FROM reviews WHERE product_id = ${prodId} AND rating = 1`, (err, result) => {
-            if (err) {
-              callback(err);
-            } else {
-              metadata['ratings']['1'] = result.rows[0].count;
-              pool.query(`SELECT COUNT (*) FROM reviews WHERE product_id = ${prodId} AND rating = 2`, (err, result) => {
-                if (err) {
-                  callback(err);
-                } else {
-                  metadata['ratings']['2'] = result.rows[0].count;
-                  pool.query(`SELECT COUNT (*) FROM reviews WHERE product_id = ${prodId} AND rating = 3`, (err, result) => {
-                    if (err) {
-                      callback(err);
-                    } else {
-                      metadata['ratings']['3'] = result.rows[0].count;
-                      pool.query(`SELECT COUNT (*) FROM reviews WHERE product_id = ${prodId} AND rating = 4`, (err, result) => {
-                        if (err) {
-                          callback(err);
-                        } else {
-                          metadata['ratings']['4'] = result.rows[0].count;
-                          pool.query(`SELECT COUNT (*) FROM reviews WHERE product_id = ${prodId} AND rating = 5`, (err, result) => {
-                            if (err) {
-                              callback(err);
-                            } else {
-                              metadata['ratings']['5'] = result.rows[0].count;
-                              //POPULATE RECOMMENDED OBJECT
-                              pool.query(`SELECT COUNT (*) FROM reviews WHERE product_id = ${prodId} AND recommend = true`, (err, result) => {
-                                if (err) {
-                                  callback(err);
-                                } else {
-                                  metadata['recommended']['true'] = result.rows[0].count;
-                                  pool.query(`SELECT COUNT (*) FROM reviews WHERE product_id = ${prodId} AND recommend = false`, (err, result) => {
-                                    if (err) {
-                                      callback(err);
-                                    } else {
-                                      metadata['recommended']['false'] = result.rows[0].count;
-                                      callback(null, metadata);
-                                    }
-                                  });
-                                }
-                              });
-                            }
-                              });
-                            }
-                          });
-                        }
-                  });
-                }
-              });
-            }
-          });
-        }
-      });
+      res.status(200).send(result);
+    }
+  }, prodId, sortBy, resultCount, resultPages);
+});
+
+app.get('/reviews/meta', (req, res) => {
+  var prodId = req.query.product_id;
+  fetchMeta(prodId, (err, result) => {
+    if (err) {
+      res.status(400).send(err);
+    } else {
+      res.status(200);
+      res.send(result);
     }
   });
-};
+});
 
-//INSERT REVIEW INTO REVIEWS TABLE
-var insertReview = (prodId, rating, summary, body, recommend, name, email, photos, characteristics, callback) => {
-
-  pool.connect();
-
-  //INSERT INTO REVIEWS TABLE
-  pool.query(`INSERT INTO reviews (product_id, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness)
-  VALUES (${prodId}, ${rating}, (EXTRACT(EPOCH FROM CURRENT_TIMESTAMP)), '${summary}', '${body}', ${recommend}, false, '${name}', '${email}', 'null', 0)`, (err, result) => {
+app.post('/reviews-table-insert', (req, res) => {
+  //req.body will access data sent in the axios req at the 'data' property
+  //set default values for body parameters that are optional
+  var prodId = req.body.product_id;
+  if (!req.body.rating) {
+    var rating = null;
+  } else {
+    var rating = req.body.rating;
+  }
+  if (!req.body.summary) {
+    var summary = null;
+  } else {
+    var summary = req.body.summary;
+  }
+  if (!req.body.body) {
+    var body = null;
+  } else {
+    var body = req.body.body;
+  }
+  if (!req.body.recommend) {
+    var recommend = null;
+  } else {
+    var recommend = req.body.recommend;
+  }
+  if (!req.body.reviewer_name) {
+    var name = null;
+  } else {
+    var name = req.body.reviewer_name;
+  }
+  if (!req.body.email) {
+    var email = null;
+  } else {
+    var email = req.body.email;
+  }
+  if (!req.body.photos) {
+    var photos = [];
+  } else {
+    var photos = req.body.photos;
+  }
+  if (!req.body.characteristics) {
+    var characteristics = null;
+  } else {
+    var characteristics = req.body.characteristics;
+  }
+  insertReview(prodId, rating, summary, body, recommend, name, email, photos, characteristics, (err, result) => {
     if (err) {
-      callback(err);
+      console.log(err);
+      res.sendStatus(404);
     } else {
-      console.log(result);
-      callback(null, result);
+      res.sendStatus(201);
+    }
+  });
+});
 
-      //INSERT PHOTO URLS INTO REVIEWS_PHOTOS TABLE
+// app.put('/reviews/:review_id/helpful', (req, res) => {
+//   var reviewId = req.params.review_id;
+//   res.send(reviewId);
+// });
 
-      // var ids = [];
-      // for (var i = 0; i < photos.length; i++) {
-      //   ids.push(prodId);
-      // }
-      // const query = 'INSERT INTO reviews_photos (product_id, url) SELECT * FROM unnest($1::INT[], $2::TEXT[])';
-      // const values = [ids, photos];
-      // console.log('query string', query);
+// app.put('/reviews/:review_id/report', (req, res) => {
+//   var reviewId = req.params.review_id;
+//   res.send(reviewId);
+// });
 
-      // pool.query(query, values, (err, result) => {
-      //   if (err) {
-      //     callback(err);
-      //   } else {
-
-          //INSERT INTO CHARACTERISTIC_REVIEW TABLE
-          //characteristics object:
-            //{ "14": 3, "15": 5, "16": 3, "17": 2}
-          //create an array of keys from characteristics object
-          // var keys = Object.keys(characteristics);
-          // var charIdOne = keys[0]
-          // pool.query(`INSERT INTO characteristic_review (characteristic_id, review_id, value) VALUES (${ }), (${ }), (${ }), (${ })`)
-        }
-      });
-    };
-  // });
-// };
-
-module.exports.fetchReviews = fetchReviews;
-module.exports.fetchMeta = fetchMeta;
-module.exports.insertReview = insertReview;
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
